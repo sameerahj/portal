599ebd63826a608fc37798996ba0c28f68f3bab6
harsha89 <harsz89@gmail.com>
2013-06-01 20:44:32 +0530

    adding kickstart to the project

---
+++ src/main/java/org/dhara/portal/web/services/VisualizationServices.java
@@ -0,0 +1,16 @@
+package org.dhara.portal.web.services;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: harsha
+ * Date: 6/1/13
+ * Time: 8:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class VisualizationServices {
+    public boolean KMLDataVisualizationService(String KMLData)
+    {
+
+      return true;
+    }
+}
