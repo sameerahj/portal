62eb6149531f3564f8322d0d48cd6e3168ab80c9
harsha89 <harsz89@gmail.com>
2013-06-17 19:24:45 +0530

    airavata configguration

---
+++ src/main/java/org/dhara/portal/web/airavataClient/AiravataConfig.java
@@ -0,0 +1,17 @@
+package org.dhara.portal.web.airavataClient;
+
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: harsha
+ * Date: 6/13/13
+ * Time: 2:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AiravataConfig {
+    public static Map<String,String> getConfig()
+    {
+
+    }
+}
